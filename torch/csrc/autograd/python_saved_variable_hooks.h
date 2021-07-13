2c647d975d587ea7d46e9c454522f8a1ec3f0796
Victor Quach <quach@fb.com>
2021-07-13 03:05:25 -0400

    Apply saved tensor hooks
    Summary: Fixes #58512
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 44cf2c1ef08b710e0b40ebf10bd98fe0530a33b9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60975

--- torch/csrc/autograd/python_saved_variable_hooks.h
+++ torch/csrc/autograd/python_saved_variable_hooks.h
@@ -4,6 +4,7 @@
 #include <torch/csrc/autograd/python_variable.h>
 #include <torch/csrc/autograd/saved_variable_hooks.h>
 #include <torch/csrc/python_headers.h>
+#include <torch/csrc/THP.h>
 #include <ATen/ATen.h>
 
 namespace py = pybind11;
@@ -16,9 +17,10 @@ struct PySavedVariableHooks : public SavedVariableHooks {
   at::Tensor call_unpack_hook() override;
   ~PySavedVariableHooks() override;
 
-private:
-  py::function pack_hook_;
-  py::function unpack_hook_;
+  private:
+    py::function pack_hook_;
+    py::function unpack_hook_;
+    PyObject* data_ = nullptr;
 };
 
 }}
